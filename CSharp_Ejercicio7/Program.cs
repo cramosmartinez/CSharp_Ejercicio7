69f4d268c610889e088baf15dd5c4d9bbe1d3812
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-07-21 17:49:40 -0600

    Agregar archivos de proyecto.

---
+++ CSharp_Ejercicio7/Program.cs
@@ -0,0 +1,8 @@
+﻿//Recorre los números del 1 al 100. Muestra los números pares. Usar el tipo de bucle que quieras.
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
